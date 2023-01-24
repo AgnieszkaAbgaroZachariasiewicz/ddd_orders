affe2e2317f3f1f3b5667e9fabd4e8ad2682b2fc
Agnieszka Zachariasiewicz <a.zachariasiewicz@gmail.com>
2023-01-24 18:55:42 +0100

    orders migration

--- db/migrate/20230122222513_create_orders.rb
+++ db/migrate/20230122222513_create_orders.rb
@@ -1,6 +1,10 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
+      t.string :uid
+      t.string :number
+      t.string :customer
+      t.string :state
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_22_222513) do
   end
 
   create_table "orders", force: :cascade do |t|
+    t.string "uid"
+    t.string "number"
+    t.string "customer"
+    t.string "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
